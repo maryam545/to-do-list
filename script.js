cf9031af79ec64b972100c60fb4d93d804598522
maryam <marychch19@gmail.com>
2025-02-18 18:50:46 +0330

    taskList

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ const add = document.querySelector(".add");
 const input = document.querySelector(".input");
 const taskList = document.querySelector(".task");
 
-add.addEventListener("click");
+add.addEventListener("click", () => {
+  if (input.value.trim() !== "") {
+    const newTask = document.createElement("li");
+    newTask.textContent = input.value;
+    taskList.appendChild(newTask);
+    input.
+  }
+});
+
+//     taskList.appendChild(newTask);
+
+//     input.value = "";
+//   }
+// });
